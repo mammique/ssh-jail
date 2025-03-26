027c393569d485f9de9cedb1ba13a455e5620104
Camille Harang <mammique@yooook.net>
2025-03-26 22:08:21 +0100

    Create jail-uninst.sh

---
+++ jail-uninst.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CONFIG_FILE="/etc/ssh/jail.conf"
+CONFIG_FILE_SSH='/etc/ssh/sshd_config.d/jail.conf'
+KEYS_PATH='/etc/ssh/jail_keys/'
+source "$CONFIG_FILE"
+
+echo
+echo "This will not remove Jail SSH accounts and environments, just the SSHd installation."
+echo
+
+read -p "Uninstall Jail SSH? [y/N] " confirm
+[[ $confirm != [yY] ]] && exit 0
+
+rm $CONFIG_FILE
+rm $CONFIG_FILE_SSH
+delgroup $JAIL_GROUP
+
+echo
+echo "Done."
+echo
