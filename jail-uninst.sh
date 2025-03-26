54e6335a569fc8cf79b1b50662b5bf3f6bbbac8c
Camille Harang <mammique@garbure.org>
2025-03-26 22:54:08 +0100

    Verbose.

--- jail-uninst.sh
+++ jail-uninst.sh
@@ -12,9 +12,11 @@ echo
 read -p "Uninstall Jail SSH? [y/N] " confirm
 [[ $confirm != [yY] ]] && exit 0
 
+echo "Removing configuration files…"
 rm $CONFIG_FILE
 rm $CONFIG_FILE_SSH
 delgroup $JAIL_GROUP
+echo "Restart SSH server…"
 systemctl restart ssh
 
 echo
